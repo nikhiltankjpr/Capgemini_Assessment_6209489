ce6e0428f252d99ba75f6139e9f4bfe701655dd2
nikhiltankjpr <nikhiltankjpr@gmail.com>
2026-03-16 16:38:38 +0530

    edited

--- Day20/pom/flipkart.page.ts
+++ Day20/pom/flipkart.page.ts
@@ -1,77 +1,37 @@
-import { Page, Locator } from '@playwright/test';
-
-export class FlipkartPage {
-
-  readonly page: Page;
-  readonly homeLink: Locator;
-  readonly gudiPadwaStore: Locator;
-  readonly gudiCloth: Locator;
-  readonly products: Locator;
-  readonly addToCartBtn: Locator;
-  readonly cartBtn: Locator;
-  readonly quantityIncrease: Locator;
-  readonly placeOrderBtn: Locator;
-
-  constructor(page: Page) {
-    this.page = page;
-
-    this.homeLink = page.getByText('Home');
-    this.gudiPadwaStore = page.getByText('Gudi Padwa');
-    this.gudiCloth = page.getByText('Gudi Cloth');
-
-    this.products = page.locator('div._1AtVbE');
-
-    this.addToCartBtn = page.getByText('Add to cart');
-
-    this.cartBtn = page.locator('a[href*="viewcart"]');
-
-    this.quantityIncrease = page.locator('button:has-text("+")');
-
-    this.placeOrderBtn = page.getByText('Place Order');
-  }
-
-  async navigate(url: string) {
-    await this.page.goto(url);
-  }
-
-  async clickHome() {
-    await this.homeLink.click();
-  }
-
-  async openGudiPadwaStore() {
-    await this.gudiPadwaStore.scrollIntoViewIfNeeded();
-    await this.gudiPadwaStore.click();
-  }
-
-  async openGudiCloth() {
-    await this.gudiCloth.click();
-  }
-
-  async addProductToCart(index: number) {
-
-    const product = this.products.nth(index);
-
-    await product.scrollIntoViewIfNeeded();
-    await product.click();
-
-    const pages = this.page.context().pages();
-    const newPage = pages[pages.length - 1];
-
-    await newPage.getByText('Add to cart').click();
-  }
-
-  async goToCart() {
-    await this.cartBtn.click();
-  }
-
-  async increaseQuantity(times: number) {
-    for (let i = 0; i < times; i++) {
-      await this.quantityIncrease.first().click();
+import {Locator,Page} from "@playwright/test"
+class Flipkart{
+    homeBtn:Locator
+    gudiStore:Locator
+    gudiCloth:Locator
+    page:Page
+    singleProduct:Locator
+    page2:Page
+    constructor(page:Page){
+        this.page=page
+        this.homeBtn=page.locator('(//div[@class="css-146c3p1 r-dnmrzs r-1udh08x r-1udbk01 r-3s2u2q r-1iln25a r-1b1g84l r-tuq35u r-u8s1d r-q4m81j r-1r0gou5"])[6]/..')
+        this.gudiStore=page.locator('(//img[@style="filter: none; opacity: 1; transition: filter 0.5s ease-in-out, opacity 0.5s ease-in-out; width: 100%; margin: auto; display: block; object-fit: contain; aspect-ratio: 1 / 1;"])[2]');
+        this.gudiCloth=page.locator('//img[@style="filter: none; opacity: 1; transition: filter 0.5s ease-in-out, opacity 0.5s ease-in-out; width: 100%; margin: auto; display: block; object-fit: contain; aspect-ratio: 3 / 4;"]').nth(1);
+        this.singleProduct=page.locator('//div[@class="RGLWAk"]').nth(4);
+        this.page2=page;
+        const 
     }
-  }
+    async getProductsPage(){
+        await this.homeBtn.waitFor({ state: "visible" })
+        await this.homeBtn.click()
 
-  async placeOrder() {
-    await this.placeOrderBtn.click();
-  }
+        await this.gudiStore.waitFor({ state: "visible" })
+        await this.gudiStore.click()
 
+        await this.gudiCloth.waitFor({ state: "visible" })
+        await this.gudiCloth.click()
+    }
+    async addProducts(){
+        const [newpage]=await Promise.all([
+            this.page.waitForEvent("popup"),
+            this.singleProduct.click()
+        ])
+        this.page2=newpage
+        
+    }
 }
+export default Flipkart;
--- Day20/tests/flipkarttask.spec.ts
+++ Day20/tests/flipkarttask.spec.ts
@@ -3,40 +3,18 @@ import { FlipkartPage } from '../pom/flipkart.page.ts';
 import data from '../testdata/data2.json';
 
 test('Flipkart', async ({ page }) => {
-
   const flipkart = new FlipkartPage(page);
-
-  // Launch URL
   await flipkart.navigate(data.url);
-
-  // Click Home
   await flipkart.clickHome();
-
-  // Scroll & Click Gudi Padwa Store
   await flipkart.openGudiPadwaStore();
-
-  // Click Gudi Cloth
   await flipkart.openGudiCloth();
-
-  // Add 5th Product
   await flipkart.addProductToCart(data.productIndexes[0]);
-
-  // Add 8th Product
   await flipkart.addProductToCart(data.productIndexes[1]);
-
-  // Go To Cart
   await flipkart.goToCart();
-
-  // Increase Quantity for both products
   await flipkart.increaseQuantity(data.quantity);
-
-  // Place Order
   await flipkart.placeOrder();
-
-  // Screenshot
   await page.screenshot({
     path: 'flipkart-order.png',
     fullPage: true
   });
-
 });
