52e6891343bd62a81cc8783372036793a98eed68
nikhiltankjpr <nikhiltankjpr@gmail.com>
2026-03-16 23:32:56 +0530

    remaining done

--- Day20/pom/flipkart.page.ts
+++ Day20/pom/flipkart.page.ts
@@ -1,19 +1,31 @@
 import {Locator,Page} from "@playwright/test"
 class Flipkart{
+    closePopup:Locator
     homeBtn:Locator
     gudiStore:Locator
     gudiCloth:Locator
     page:Page
     singleProduct:Locator
+    singleProduct2:Locator
     page2:Page
+    cart:Locator
     constructor(page:Page){
         this.page=page
+        this.closePopup=page.locator('//span[@class="b3wTlE"]')
         this.homeBtn=page.locator('(//div[@class="css-146c3p1 r-dnmrzs r-1udh08x r-1udbk01 r-3s2u2q r-1iln25a r-1b1g84l r-tuq35u r-u8s1d r-q4m81j r-1r0gou5"])[6]/..')
-        this.gudiStore=page.locator('(//img[@style="filter: none; opacity: 1; transition: filter 0.5s ease-in-out, opacity 0.5s ease-in-out; width: 100%; margin: auto; display: block; object-fit: contain; aspect-ratio: 1 / 1;"])[2]');
-        this.gudiCloth=page.locator('//img[@style="filter: none; opacity: 1; transition: filter 0.5s ease-in-out, opacity 0.5s ease-in-out; width: 100%; margin: auto; display: block; object-fit: contain; aspect-ratio: 3 / 4;"]').nth(1);
+        this.gudiStore=page.locator('//a[@href="/gudipadwa2026-at-store?param=7561&BU=Home"]');
+        this.gudiCloth=page.locator('//a[@href="/all/~cs-bjn56jm6a4/pr?sid=all&collection-tab-name=Gudi+padwa+&pageCriteria=default&Param=356643&BU=Home"]');
         this.singleProduct=page.locator('//div[@class="RGLWAk"]').nth(4);
+        this.singleProduct2=page.locator('//div[@class="RGLWAk"]').nth(7);
         this.page2=page;
-        const 
+        this.cart=page.locator("//a[@class='WGWdFn']")
+        
+    }
+    async handlePopupDialog(){
+
+        if(await this.closePopup.isVisible()){
+        await this.closePopup.click();
+    }
     }
     async getProductsPage(){
         await this.homeBtn.waitFor({ state: "visible" })
@@ -26,12 +38,33 @@ class Flipkart{
         await this.gudiCloth.click()
     }
     async addProducts(){
-        const [newpage]=await Promise.all([
+        const [product1page]=await Promise.all([
             this.page.waitForEvent("popup"),
             this.singleProduct.click()
         ])
-        this.page2=newpage
+        this.page2=product1page
+        await this.page2.waitForLoadState()
+        await this.page2.getByText("Add to cart").click();
         
+        await this.page2.close()
+        const [product2page]=await Promise.all([
+            this.page.waitForEvent("popup"),
+            this.singleProduct2.click()
+        ])
+
+        this.page2=product2page
+        await this.page2.waitForLoadState()
+        await this.page2.getByText("Add to cart").click();
+        await this.page2.close()
+    }
+    async manageQuantity(){
+        await this.cart.click();
+        await this.page.locator('//input[@class="j93Ywx"]').first().fill("3");
+        await this.page.locator('//input[@class="j93Ywx"]').last().fill("2");
+        await this.page.screenshot({path:"/screenshot/FlipkartScenario.png"})
+        await this.page.locator('//button[@class="dSM5Ub JMrpad KcXDCU"]').click()
+
+
     }
 }
 export default Flipkart;
--- Day20/tests/flipkarttask.spec.ts
+++ Day20/tests/flipkarttask.spec.ts
@@ -1,20 +1,10 @@
-import { test } from '@playwright/test';
-import { FlipkartPage } from '../pom/flipkart.page.ts';
-import data from '../testdata/data2.json';
-
-test('Flipkart', async ({ page }) => {
-  const flipkart = new FlipkartPage(page);
-  await flipkart.navigate(data.url);
-  await flipkart.clickHome();
-  await flipkart.openGudiPadwaStore();
-  await flipkart.openGudiCloth();
-  await flipkart.addProductToCart(data.productIndexes[0]);
-  await flipkart.addProductToCart(data.productIndexes[1]);
-  await flipkart.goToCart();
-  await flipkart.increaseQuantity(data.quantity);
-  await flipkart.placeOrder();
-  await page.screenshot({
-    path: 'flipkart-order.png',
-    fullPage: true
-  });
-});
+import {test} from "@playwright/test"
+import Flipkart from "../pom/flipkart.page.ts"
+test("Flipkart Scenario",async({page})=>{
+    await page.goto("https://Flipkart.com");
+    const flipkartPage=new Flipkart(page);
+    await flipkartPage.handlePopupDialog();
+    await flipkartPage.getProductsPage();
+    await flipkartPage.addProducts();
+    await flipkartPage.manageQuantity()
+})
